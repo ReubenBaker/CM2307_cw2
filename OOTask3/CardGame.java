d9bdd220ba22622d8bd8670e2a59a7a526bdb9f1
ReubenBaker <baker.reuben@outlook.com>
2023-05-03 06:13:26 +0100

    Updated card game to use tscd

--- OOTask3/CardGame.java
+++ OOTask3/CardGame.java
@@ -1,49 +1,20 @@
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 
 public class CardGame {
-    private ArrayList<String> deck;
-    private RandomInterface r;
+    private ThreadSafeCardDeck deck;
     private HashSet<String> cardsChosen;
     private final Scanner scanner;
-    private final int numberOfShuffles = 100;
 
     public CardGame(Scanner scanner) {
-        this.deck = new ArrayList<String>();
-        this.r = new LinearCongruentialGenerator();
+        this.deck = new ThreadSafeCardDeck();
         this.cardsChosen = new HashSet<String>();
         this.scanner = scanner;
     }
 
     public Boolean play() {
-        initialiseDeck();
-        shuffleDeck();
         chooseCards();
         return declareWinner();
-    }
-
-    private void initialiseDeck() {
-        String[] suits = { "Hearts", "Diamonds", "Spades", "Clubs" };
-        String[] ranks = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
-
-        for (String suit : suits) {
-            for (String rank : ranks) {
-                deck.add(rank + suit);
-            }
-        }
-    }
-
-    private void shuffleDeck() {
-        for (int i = 0; i < numberOfShuffles; i++) {
-            int firstIndex = ((int) (r.next() * 52));
-            int secondIndex = ((int) (r.next() * 52));
-            String temp = deck.get(firstIndex);
-            deck.set(firstIndex, deck.get(secondIndex));
-            deck.set(secondIndex, temp);
-        }
-
-        System.out.println(deck);
     }
 
     private void chooseCards() {
@@ -55,13 +26,13 @@ public class CardGame {
                 e.printStackTrace();
             }
 
-            int cardChoice = (int) (r.next() * deck.size());
-            System.out.println("You chose " + deck.get(cardChoice));
-            cardsChosen.add(deck.remove(cardChoice));
+            String cardChoice = deck.dealCard();
+            System.out.println("You chose " + cardChoice);
+            cardsChosen.add(cardChoice);
         }
 
         scanner.close();
-        
+
         System.out.println("Cards chosen: " + cardsChosen);
         System.out.println("Remaining cards: " + deck);
     }
