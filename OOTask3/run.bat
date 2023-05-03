6b689b27fc0af1ef10ce03f59fbcd5ac28d54fae
ReubenBaker <baker.reuben@outlook.com>
2023-05-03 06:30:29 +0100

    Updated run

--- OOTask3/run.bat
+++ OOTask3/run.bat
@@ -1,2 +1,2 @@
-javac -d ./class -cp ../OOTask2 ../OOTask2/ThreadSafeCardDeck.java ./*.java
+javac -d ./class -cp ../OOTask2:. ../OOTask2/ThreadSafeCardDeck.java ./*.java
 java -cp ./class ImprovedGame
